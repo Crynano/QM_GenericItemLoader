9db7071b261b030901825741f06bc7488c8ee014
Crynano <pa.crysis@gmail.com>
2025-02-22 04:21:59 +0100

    Add project files.

---
+++ QM_GenericItemLoader/src/Main.cs
@@ -0,0 +1,23 @@
+﻿using MGSC;
+using QM_WeaponImporter;
+using System.IO;
+using System.Reflection;
+
+namespace QM_ExpandedFactionArsenal
+{
+    public static class Main
+    {
+        private static string AssemblyFolder => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        [Hook(ModHookType.AfterConfigsLoaded)]
+        public static void AfterConfig(IModContext context)
+        {
+            LoadItems();
+        }
+
+        private static void LoadItems()
+        {
+            API.LoadModConfig("QM_ExpandedFactionArsenal", AssemblyFolder);
+        }
+    }
+}
