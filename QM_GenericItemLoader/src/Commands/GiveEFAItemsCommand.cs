ad0de1980871712fef4781c545cb5659d8427908
Crynano <pa.crysis@gmail.com>
2025-03-12 22:39:47 +0100

    Added two unlock commands for the game

---
+++ QM_GenericItemLoader/src/Commands/GiveEFAItemsCommand.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using MGSC;
+
+namespace QM_ExpandedFactionArsenal
+{
+    [ConsoleCommand(new string[] { "allefaitems" })]
+    public class GiveEFAItemsCommand
+    {
+        [Inject(false)]
+        private readonly MagnumCargo _magnumCargo;
+
+        [Inject(false, AllowNull = true)]
+        private readonly Creatures _creatures;
+
+        [Inject(false, AllowNull = true)]
+        private readonly ItemsOnFloor _itemsOnFloor;
+
+        public static string Help(string command, bool verbose)
+        {
+            return "Spawn all Expanded Faction Armory items.";
+        }
+
+        public string Execute(string[] tokens)
+        {
+            try
+            {
+                var listOfEFAWeapons = Data.Items.Ids.ToList().Where(x => x.StartsWith("efa_"));
+                foreach(var weaponId in listOfEFAWeapons)
+                {
+                    BasePickupItem basePickupItem = SingletonMonoBehaviour<ItemFactory>.Instance.CreateForInventory(weaponId);
+                    if (SingletonMonoBehaviour<DungeonGameMode>.Instance == null)
+                    {
+                        _magnumCargo.ShipCargo[0].AddItemAndReshuffleOptional(basePickupItem);
+                    }
+                    else
+                    {
+                        Player player = _creatures.Player;
+                        ItemOnFloorSystem.SpawnItem(_itemsOnFloor, basePickupItem, player.CreatureData.Position);
+                    }
+                }
+                return "Added all EFA weapons successfully.";
+            }
+            catch (NullReferenceException exception)
+            {
+                string msg = $"ERROR: Something unexpected happened.";
+                Debug.Log(msg);
+                return msg;
+            }
+        }
+
+        public static List<string> FetchAutocompleteOptions(string command, string[] tokens)
+        {
+            return null;
+        }
+
+        public static bool IsAvailable()
+        {
+            return true;
+        }
+
+        public static bool ShowInHelpAndAutocomplete()
+        {
+            return true;
+        }
+    }
+}
---
+++ QM_GenericItemLoader/src/Commands/UnlockEFARecipesCommand.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using MGSC;
+
+namespace QM_ExpandedFactionArsenal
+{
+    [ConsoleCommand(new string[] { "allefachips" })]
+    public class UnlockEFARecipesCommand
+    {
+        [Inject(false)]
+        private readonly MagnumCargo _magnumCargo;
+
+        public static string Help(string command, bool verbose)
+        {
+            return "Unlock all Expanded Faction Arsenal (EFA) crafting recipes.";
+        }
+
+        public string Execute(string[] tokens)
+        {
+            try
+            {
+                var listOfEFAWeapons = Data.Items.Ids.ToList().Where(x => x.StartsWith("efa_"));
+                foreach (var weaponId in listOfEFAWeapons)
+                {
+                    if (!_magnumCargo.UnlockedProductionItems.Contains(weaponId)) _magnumCargo.UnlockedProductionItems.Add(weaponId);
+                }
+                return "Unlocked all Expanded Faction Arsenal (EFA) crafting recipes successfully.";
+            }
+            catch (NullReferenceException exception)
+            {
+                string msg = $"ERROR: Something unexpected happened.";
+                Debug.Log(msg);
+                return msg;
+            }
+        }
+
+        public static List<string> FetchAutocompleteOptions(string command, string[] tokens)
+        {
+            return null;
+        }
+
+        public static bool IsAvailable()
+        {
+            return true;
+        }
+        public static bool ShowInHelpAndAutocomplete()
+        {
+            return true;
+        }
+    }
+}
