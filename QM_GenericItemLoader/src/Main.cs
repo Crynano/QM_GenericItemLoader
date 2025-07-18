59e39bac5d9a2ade911d942f7ae71f5889784c8c
Crynano <pa.crysis@gmail.com>
2025-07-18 20:56:12 +0200

    v1.0.1

--- QM_GenericItemLoader/src/Main.cs
+++ QM_GenericItemLoader/src/Main.cs
@@ -1,23 +1,14 @@
 ﻿using MGSC;
 using QM_WeaponImporter;
-using System.IO;
-using System.Reflection;
 
 namespace QM_MalorianArms
 {
     public static class Main
     {
-        private static string AssemblyFolder => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
         [Hook(ModHookType.AfterConfigsLoaded)]
         public static void AfterConfig(IModContext context)
         {
-            LoadItems();
-        }
-
-        private static void LoadItems()
-        {
-            API.LoadModConfig("QM_MalorianArms", AssemblyFolder);
+            API.LoadModConfig("QM_MalorianArms", context);
         }
     }
 }
