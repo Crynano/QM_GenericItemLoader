31796cf7747586a4f5c49fa52bae9d9dcccd81eb
Crynano <pa.crysis@gmail.com>
2026-08-16 21:37:34 +0200

    GenericItemLoader

--- QM_GenericItemLoader/src/Main.cs
+++ QM_GenericItemLoader/src/Main.cs
@@ -1,23 +1,16 @@
 ﻿using MGSC;
-using QM_WeaponImporter;
 using System.IO;
 using System.Reflection;
+using QM_ImporterAPI.Services;
 
 namespace QM_GenericItemLoader
 {
     public static class Main
     {
-        private static string AssemblyFolder => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-
         [Hook(ModHookType.AfterConfigsLoaded)]
         public static void AfterConfig(IModContext context)
         {
-            LoadItems();
-        }
-
-        private static void LoadItems()
-        {
-            API.LoadModConfig("QM_GenericItemLoader", AssemblyFolder);
+            ImporterApi.LoadModFromContext(context);
         }
     }
 }
--- QM_GenericItemLoader/src/Properties/AssemblyInfo.cs
+++ QM_GenericItemLoader/src/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QM_GenericItemLoader")]
+[assembly: AssemblyTitle("QM_CMC_GenericItemLoader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QM_GenericItemLoader")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyProduct("QM_CMC_GenericItemLoader")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
